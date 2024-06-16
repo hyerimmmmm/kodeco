50310be51c81ab6bc4398ba4451a77f69465cfd4
hyerim <ax1021@naver.com>
2024-06-16 17:47:48 +0900

    The One-Button App

--- UIKitApprentice/BullsEye/BullsEye/ViewController.swift
+++ UIKitApprentice/BullsEye/BullsEye/ViewController.swift
@@ -13,6 +13,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showAlert() {
+        // 경고창 생성
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: "This is my first app!",
+                                      preferredStyle: .alert)
+        
+        // 경고창 동작 생성
+        let action = UIAlertAction(title: "Awesome",
+                                   style: .default,
+                                   handler: nil)
+        
+        // 경고창에 동작 추가
+        alert.addAction(action)
+        // 버튼 누르면 경고창 띄우기
+        present(alert, animated: true, completion: nil)
+    }
 
 
 }

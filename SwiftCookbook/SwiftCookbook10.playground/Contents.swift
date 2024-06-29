203e6e5e5ee1155e5de99ed98160e91b601437ab
hyerim <ax1021@naver.com>
2024-06-29 18:49:40 +0900

    [Swift Cookbook] 키 경로 사용하기

--- SwiftCookbook/SwiftCookbook10.playground/Contents.swift
+++ SwiftCookbook/SwiftCookbook10.playground/Contents.swift
@@ -1,27 +1,22 @@
 import UIKit
 
 
-// MARK: - 구조체 VS 클래스
-// 클래스 사용: 복잡해질 가능성이 있는 내용
-// 사람에 관한 데이터를 저장하는데 사용하는 경우, 다양한 곳에 데이터가 중복 상속될 가능성이 높기 때문에 상속이 가능한 클래스 사용이 유리
-class Person {
+// MARK: - 키 경로 사용하기
+// 구조체 생성
+struct Person {
     var name: String
-    var agg: Int
-    
-    init(name: String, agg: Int) {
-        self.name = name
-        self.agg = agg
-    }
+    var address: Address
 }
 
-// 구조체 사용: 단순하게 정리되는 내용
-// 성적 정보를 저장하는 단순한 정보이기 때문에 구조체 사용이 유리
-struct Grade {
-    var letter: String
-    var points: Double
-    
-    init(letter: String, points: Double) {
-        self.letter = letter
-        self.points = points
-    }
+struct Address {
+    var street: String
+    var city: String
 }
+
+// 인스턴스 생성
+let person = Person(name: "John Doe", address: Address(street: "1 Main St", city: "San Francisco"))
+
+// 키 경로 변수에 저장
+let streetKeyPath = \Person.address.street
+// 키 경로에 대한 값에 접근
+let street = person[keyPath: streetKeyPath] // output: "1 Main St" / person이라는 인스턴스가 키 경로 위치에 가지고 있는 값에 접근

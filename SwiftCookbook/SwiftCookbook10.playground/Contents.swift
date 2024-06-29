a5c13a2fdad076f428d9a48f81db236048b36833
hyerim <ax1021@naver.com>
2024-06-29 17:34:04 +0900

    [Swift Cookbook] 다형성 사용하기

--- SwiftCookbook/SwiftCookbook10.playground/Contents.swift
+++ SwiftCookbook/SwiftCookbook10.playground/Contents.swift
@@ -1,42 +1,36 @@
 import UIKit
 
-// MARK: - 상속 사용하기
+// MARK: - 다형성 사용하기
 // 수퍼클래스(부모클래스) 생성
-class Vehicle {
-    // 고유 속성 생성
-    var wheels: Int
-    var weight: Double
-    var maxSpeed: Double
-    
-    // 속성 초기화
-    init(wheels: Int, weight: Double, maxSpeed: Double) {
-        self.wheels = wheels
-        self.weight = weight
-        self.maxSpeed = maxSpeed
-    }
-    
-    // 고유 메소드 생성
-    func description() -> String {
-        return "A vehicle with \(wheels) wheels, weighs \(weight) kgs and has a maximum speed of \(maxSpeed) km/h."
+class Shape {
+    func draw() {
+        print("Drawing a shape")
     }
 }
 
-// 클래스 Vehicle를 상속받는 서브클래스 생성
-class Car: Vehicle {
-    // 고유 속성 생성
-    var numberOfDoors: Int // 클래스 내부에서 부르는 이름
-    
-    init(wheels: Int, weight: Double, maxSpeed: Double, doors: Int) { // doors는 numberOfDoors를 클래스 밖에서 부르는 이름
-        // 고유 속성 초기화
-        self.numberOfDoors = doors
-        // 수퍼클래스(부모클래스) 초기화
-        super.init(wheels: wheels, weight: weight, maxSpeed: maxSpeed)
+// 서브클래스(자식클래스) 생성
+class Circle: Shape {
+    // override를 사용해 수퍼클래스(부모클래스)에서 정의 된 함수의 실행 코드 변경
+    override func draw() {
+        print("Drawing a circle")
     }
 }
 
-// 서브클래스 객체(인스턴스) 생성
-let myCar = Car(wheels: 4, weight: 1000, maxSpeed: 200, doors: 4) // 수퍼클래스에 있던 속성들도 사용 가능
+// 서브클래스(자식클래스) 생성
+class Square: Shape {
+    // override를 사용해 수퍼클래스(부모클래스)에서 정의 된 함수의 실행 코드 변경
+    override func draw() {
+        print("Drawing a square")
+    }
+}
 
-// 서브클래스에서 수퍼클래스의 속성 및 메소드에 접근하기
-print(myCar.wheels) // output: 4
-print(myCar.description()) //output: "A vehicle with 4 wheels, weighs 1000 kgs and has a maximum speed of 200 km/h."
+// 함수 생성
+// Shape 클래스를 매개변수로 받는 함수 생성
+func drawShape(shape: Shape) {
+    // 함수가 시행되면 매개변수가 가지고 있는 draw함수 시행
+    shape.draw()
+}
+// 서브클래스를 활용한 인스턴스 생성
+// 매개변수로 불려온 클래스 뒤에 ()가 붙는 이유는 초기화 값을 불러오기 위해서
+drawShape(shape: Circle()) // output: "Drawing a circle" / 매개변수로 받아온 Circle 클래스 안에 있는 draw 함수가 시행되기 때문
+drawShape(shape: Square()) // output: "Drawing a square" / 매개변수로 받아온 Square 클래스 안에 있는 draw 함수가 시행되기 때문

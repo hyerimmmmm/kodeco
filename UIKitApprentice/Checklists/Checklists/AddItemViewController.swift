8b3cdbc33ed166ce63627fbfe623bf58160d687b
hyerim <ax1021@naver.com>
2024-07-08 16:13:13 +0900

    [UIKit] Navigation Controller: Make your own view controller class

---
+++ UIKitApprentice/Checklists/Checklists/AddItemViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by 김혜림 on 7/8/24.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}

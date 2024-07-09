a719b290daa5c1d9550396536ad52d81cc85a164
hyerim <ax1021@naver.com>
2024-07-09 14:40:50 +0900

    [Swift Cookbook] 함수와 매개변수: 정적 함수와 인스턴스 함수의 차이점

--- SwiftCookbook/SwiftCookbook10.playground/Contents.swift
+++ SwiftCookbook/SwiftCookbook10.playground/Contents.swift
@@ -1,29 +1,31 @@
 import UIKit
 
 
-// MARK: - 정적 함수와 속성의 이해
+// MARK: - 정적 함수와 인스턴스 함수의 차이점
 
-// 정적 함수 사용 예시
-struct Math {
-    static func square(number: Int) -> Int {
-        return number * number
+// 정적 함수와 인스턴스 함수 차이점 예시
+class Car {
+    // 정적 함수와 속성 선언
+    static var manufacturer = "Unknown"
+    static func describeManufacturer() -> String {
+        return "The manufacturer is \(manufacturer)."
     }
-}
-
-let result = Math.square(number: 4) // static을 사용하지 않으면 인스턴스를 생성해야지만 클래스나 구조체 안의 함수나 속성 호출 가능
-print(result) // output: 16
-
-// 정적 속성 사용 예시
-class Counter {
-    static var count = 0
     
-    init() {
-        Counter.count += 1
+    // 인스턴스 함수와 속성 선언
+    var color = "Unknown"
+    func describeColor() -> String {
+        return "The color is \(color)."
     }
 }
 
-let c1 = Counter()
-print(Counter.count) // output: 1
-let c2 = Counter()
-print(Counter.count) // output: 2
-// 모든 인스턴스가 속성을 공유하기 때문에 처음 호출 되었을 때 변경된 값이 그 다음에 호출 되었을 때에 적용되어서 값 변경
+// 정적 속성 및 함수 활용(타입에서 바로 접근 가능)
+Car.manufacturer = "Toyota"
+print(Car.describeManufacturer()) // output: "The manufacturer is Toyota."
+
+// 인스턴스 속성 및 함수 활용(인스턴스를 생성해야 접근 가능)
+let redCar = Car()
+redCar.color = "Red" // 정적 속성 처럼 클래스에서 바로 접근하려고 할 경우 에러 발생
+let greenCar = Car()
+greenCar.color = "Green"
+print(redCar.describeColor()) // output: "The color is Red."
+print(greenCar.describeColor()) // output: "The color is Green."

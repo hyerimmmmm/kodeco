b8d3d3561997078e9f735c4af5d1a9065dba4530
hyerim <ax1021@naver.com>
2024-09-12 10:15:33 +0900

    [Swfit Cookbook] Swift 제네릭에서 여러 타입 매개변수 사용하기

--- SwiftCookbook/SwiftCookbook10.playground/Contents.swift
+++ SwiftCookbook/SwiftCookbook10.playground/Contents.swift
@@ -1,31 +1,10 @@
 import UIKit
 
 
-// MARK: - 스위프트에서의 제네릭 타입
+// MARK: - Swift 제네릭에서 여러 타입 매개변수 사용하기
 
-// 제네릭 정의 예시
-struct Stack<Element> { // 단일 타입 매개변수를 가지는 Stack 이라는 구조체 생성
-    var items = [Element]()
-
-    mutating func push(_ item: Element) {
-        items.append(item)
-    }
-
-    mutating func pop() -> Element? {
-        return items.popLast()
-    }
-}
-
-var stackOfStrings = Stack<String>() // 구조체 인스턴스 생성 시 매개변수의 데이터 타입 지정
-stackOfStrings.push("Hello") // 지정된 매개변수 타입과 같은 타입의 데이터 입력
-stackOfStrings.push("World")
-if let item = stackOfStrings.pop() {
-    print(item)
-}
-
-var stackOfInts = Stack<Int>()
-stackOfInts.push(1)
-stackOfInts.push(2)
-if let int = stackOfInts.pop() {
-    print(int)
+// 제네릭에서 여러개의 타입 매개변수 사용하기
+struct Pair<T, U> {
+    let frist: T
+    let second: U
 }

b68581621eb5a9a88f43c1226d83ac0580c2737b
hyerim <ax1021@naver.com>
2024-10-21 23:22:59 +0900

    [Swift Cookbook] 제네릭을 사용해 타입 확장하기 초기화

--- SwiftCookbook/SwiftCookbook10.playground/Contents.swift
+++ SwiftCookbook/SwiftCookbook10.playground/Contents.swift
@@ -2,5 +2,14 @@ import UIKit
 
 // MARK: - 제네릭을 사용해 타입 확장하기
 
+class Point{
+    var x: Double
+    var y: Double
 
+    init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+}
 
+var point = Point(x: 10.0, y: 20.0)

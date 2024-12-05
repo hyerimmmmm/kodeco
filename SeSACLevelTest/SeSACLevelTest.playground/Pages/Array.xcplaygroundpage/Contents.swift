00f04c9928095e697bf3bb1e666ff19390a3a2b4
hyerim <ax1021@naver.com>
2024-12-05 19:57:36 +0900

    [SeSACLevelTest] 배열(1)

--- SeSACLevelTest/SeSACLevelTest.playground/Pages/Array.xcplaygroundpage/Contents.swift
+++ SeSACLevelTest/SeSACLevelTest.playground/Pages/Array.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
 let count = Int.random(in: 1...100)
 let runtime = count * 120
 let genreList = ["로맨스", "SF", "스릴러", "액션", "코미디"]
-let genre = genreList.randomElement()
+let genre = genreList.randomElement()!
 let rate: Double = .random(in: 0...5)
 
 print("""
@@ -13,3 +13,18 @@ print("""
 좋아하는 장르는 \(genre)이며
 지금까지 총 영화 평점은 \(rate)입니다.
 """)
+
+var day = ["월", "화", "수", "금", "토", "일"]
+day.insert("목", at: 3)
+print(day)
+
+day.append("목")
+print(day)
+
+day.remove(at: 3)
+print(day)
+
+print(day[0])
+
+day[6] = "화"
+print(day)
